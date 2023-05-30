5300485779db8f27cf00ae6fd812e9258a3635cb
devon gibson <devongibs2@gmail.com>
2023-05-29 23:57:18 -0700

    changes

--- src/components/logn.js
+++ src/components/logn.js
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+ import { Button } from "bootstrap";
  
  
  
@@ -9,59 +9,60 @@ const  Logn= () =>{
         const [username, setUsername] = useState();
         const [password, setPassword]= useState();
 
-      const navigate= useNavigate();
-       navigate('/admission') ;  
-
-       function handleClick() { 
-        navigate('/admission');
+       const handleSubmit=(e)=>{
+        e.preventDefault();
+         
+       };
 
         
 
-      }
+      
     return(
-         
-          
-         
-        <>
-           
+        
         <div className="Auth-form-container">
-        <form className="Auth-form">
-    <div className="Auth-form-content">
-    <div className="form-group mt-3">
-        <label>Username</label>
-        <input type= "text"  name="username" required
-        className= "form-control mt-1"
-        placeholder = "Enter username"></input>
+        <form onSubmit={handleSubmit} autocomplete="off" className="Auth-form">
+           <h1>Sign-in</h1>
+           <div className="flex-container form-group row">
+           <label className="col-sm-3 col-form-label">Username</label>
+           <input className="form-control form-control-sm"
+           type="username"
+           name="username"
+           value={username}
+           placeholder="Enter Username..."
+           onChange={(e)=> setUsername(e.target.value)}
+   />
+         </div>
+         <div className="flex-container form-group row">
+          <label className="col-sm-3 col-form-label"> Password</label>
+          <input className="form-control form-control-sm"
+           type="password"
+           name="password"
+           value={password}
+           placeholder="Enter a strong password"
+           onChange={(e) => setPassword(e.target.value)}
+   /> 
+
+   </div>
+
+
+   <button className="btn btn-primary">Log In</button>
+           
+     </form>
         
         
-        </div>
-        <div className="form-group mt-3"> 
-        <label>Password</label>
-        <input type="password" name="password" required 
-               id="pwd"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-          title="password must contain five or more characters with at least one number, one
-          uppercase and lowercase letter "
-        className="form-control mt-1"
-        placeholder  = "Enter Password"
-            
         
-          
-        />
-        
+          </div>
          
-        </div>
-          <div className="flex-container"> 
-              <button type="button" className="btn btn-primary "    onClick  >Log In</button>
-              
-            </div>
-             </div>
-             </form> 
-            
-      </div>
         
-  </>
-    )
-}
+            
+             
+            
+     
+       );
+       };
+  
+    
+ 
  
 
 export default Logn;
