cbbcddc685aa28f442d4c3613e964bee96810c98
devon gibson <devongibs2@gmail.com>
2023-06-08 21:10:12 -0700

    updates

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import { Image } from './'
+import backgroundImage from '../images/background.jpg';
+import  Image from '../images/college-classroom.jpg';
+import Image2 from '../images/college-library.jpg';
 
 
 const About = ()=> {
@@ -9,17 +11,17 @@ const About = ()=> {
           url: backgroundImage
         },
         {
-           url:"C:/Users/devon/Downloads/college-images/college-classroom.jpg"
+           url:Image
         },
         {
-            url:"C:/Users/devon/Downloads/college-images/college-library.jpg"
+            url:Image2
         },
     ];
 
     return(
         <>
      <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative'>
-        <div style={{backgroundImage:`url(${slides[0].url})`}}className='w-full h-full rounded-2xl bg-center bg-cover duration 500'>
+        <div style={{backgroundImage:`url(${slides[1].url})`}}className='w-full h-full rounded-2xl bg-center bg-cover duration 500'>
             </div>
              
             </div>
